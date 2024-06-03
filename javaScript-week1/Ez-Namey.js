297e17b214fd794906b41b142f5c4dc71d1778e1
Agyekum Dennis <kinggbadi@gmail.com>
2024-06-03 19:44:51 +0200

    Goodboy-Oldboy (A dog age calculator)

---
+++ javaScript-week1/Ez-Namey.js
@@ -0,0 +1,31 @@
+let firstWords = [
+    "Alpha",
+    "Bravo",
+    "Charlie",
+    "Delta",
+    "Echo",
+    "Foxtrot",
+    "Golf",
+    "Hotel",
+    "India",
+    "Juliet",
+];
+let secondWords = [
+    "Kilo",
+    "Lima",
+    "Mike",
+    "November",
+    "Oscar",
+    "Papa",
+    "Quebec",
+    "Romeo",
+    "Sierra",
+    "Tango",
+];
+
+const randomNumber1 = Math.floor(Math.random() * 10); 
+const randomNumber2 = Math.floor(Math.random() * 10); 
+const startupName = firstWords[randomNumber1] + " " + secondWords[randomNumber2];
+console.log(
+  `"The startup: ${startupName}, contains ${startupName.length} characters`
+);
