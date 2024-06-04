5b60b44d0d176c8f38893c736a914a0fa4ca23cd
Agyekum Dennis <kinggbadi@gmail.com>
2024-06-04 23:51:49 +0200

    Goodboy-Oldboy (A dog age calculator)

--- javaScript-week1/Goodboy-Oldboy.js
+++ javaScript-week1/Goodboy-Oldboy.js
@@ -1,14 +1,14 @@
 // Goodboy-Oldboy
 
 const dogYearOfBirth = 2020;
-let dogYearFuture = 2030;
+const dogYearFuture = 2030;
 let dogYear = dogYearFuture - dogYearOfBirth;
 let shouldShowResultInDogYears = false;
 
-if (shouldShowResultInDogYears === false) {
+if (shouldShowResultInDogYears) {
     dogYear *= 7;
   console.log(
-    `Your dog will be ${dogYear} human years old in ${dogYearFuture}`);
+    `Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
 } else {
-  console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
+  console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`);
 }
