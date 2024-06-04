56decd91b4522a3102cc6313010604ca1e4a9f71
Agyekum Dennis <kinggbadi@gmail.com>
2024-06-04 23:50:19 +0200

    Age-ify (A future age calculator)

--- javaScript-week1/Age-ify.js
+++ javaScript-week1/Age-ify.js
@@ -1,7 +1,7 @@
 // Age-ify
 
 const yearOfBirth = 1989;
-let yearFuture = 2028;
+const yearFuture = 2028;
 const age =  yearFuture - yearOfBirth;
 console.log(age)
 console.log(`You will be ${age} years old in ${yearFuture}.`)
