d9b6726ed195251ce0480df10eaf8a52aa12da7e
Dennis Agyekum <158220238+DennisAgyekum@users.noreply.github.com>
2024-06-15 17:33:58 +0200

    Update flightBooking.js
    I think  it is fixed.

--- javaScript-week2/flightBooking.js
+++ javaScript-week2/flightBooking.js
@@ -1,19 +1,19 @@
-function getFullname(firstName, surname, useFormalName, isFemale) {
-    const fullName = `${firstName} ${surname}`;
+function getFullname(firstName, surname, useFormalName, gender) {
+  const fullName = `${firstName} ${surname}`;
   if (!firstName && !surname) {
     return alert("Please enter your name");
-  } else if (useFormalName) {
-    if (!isFemale) {
-        return `Lord ${fullName}`;
-    } else if (isFemale) {
-      return `Madam ${fullName}`;
-    }
-  } else {
-    return `${fullName}`;
   }
+  if (useFormalName) {
+    if (gender === "female") {
+      return `Madam ${fullName}`;
+    } else if(gender === "male"){
+      return `Lord ${fullName}`;
+    } else {
+      return `${fullName}`
+    }
+  } else return `${fullName}`;
 }
 
-
-const printOut = getFullname("Dennis", "Agyekum", true, false);
+const printOut = getFullname("Dennis", "Agyekum", true, "");
 
 console.log(printOut);
