a2e66ca26c66d8aa0944cc8887432a7d4399d0fd
Agyekum Dennis <kinggbadi@gmail.com>
2024-06-20 23:19:24 +0200

    Javascript-warmup.js

---
+++ javascript-javascript1-week3/Javascript-warmup.js
@@ -0,0 +1,17 @@
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+
+// Write some code here
+const nameToRemove = names.splice(1, 1);
+// Code done
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+console.log(nameToRemove); // [
